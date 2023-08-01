1d4e1280c3a76cba63b0a90d27ada795167d3031
toofanjhala <jhalatoofan@gmail.com>
2023-08-01 15:03:12 +0530

    routes in middlewear

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 
 
 const Express = require('express')
+const Bodyparser = require('body-parser')
 
 const App = Express()
 
-App.use((res, req, next) => {
+App.use(Bodyparser.urlencoded({ extended: false }))
 
-    next()
+App.use('/add', (req, res, next) => {
+    res.send(`<h1>Hello from Add To Card</h1><br> 
+    <form action="/product" Method="Post" ><input type="text" name="title"></input> 
+    <input type="number" name="number"/><button type=submit>Add To Cart</button></form>`)
+
 })
 
-App.use((res, req, next) => {
-    req.send("<h1>Hello from Express</h1>")
+App.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect("/")
+})
+
+App.use("/", (req, res, next) => {
+    res.send("<h1>Hello from Express</h1>")
 })
 
 
